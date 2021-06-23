f2137a3367b54618fe5ca559567892eb59eed771
CRT_HAO <31580253+iqv29@users.noreply.github.com>
2021-06-24 02:53:27 +0800

    優化目錄結構

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='pyMobileRobotics',
+      version='0.0.1',
+      description='WorldSkills Mobile Robotics System',
+      author='CISH Robotics',
+      author_email='crt@cish.xyz',
+      url='https://github.com/CISH-Robotics',
+      keywords='robot worldskills mobile robotics',
+      project_urls={
+            'Source': 'https://github.com/CISH-Robotics/pyMobileRobotics',
+      },
+      packages=['pyMobileRobotics'],
+      install_requires=['oclock>=1.3.0', 'coloredlogs>=15.0.1', 'opencv-contrib-python>=4.1.0.25'],
+      python_requires='>=3'
+     )
